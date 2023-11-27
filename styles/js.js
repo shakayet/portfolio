013e4b88787f7960c6c038c849fc356ddee82b76
shakayet <srabonshakhawat@gmail.com>
2023-11-27 17:42:13 +0600

    upadted portfolio

---
+++ styles/js.js
@@ -0,0 +1,17 @@
+var sum1=10;
+var sum2=20;
+
+document.write("hi");
+
+if(sum1 < sum2){
+  document.write(sum2);
+}
+else if(sum2< sum1){
+    document.write("i am back");
+}
+else if{
+    document.write("she is beautifull");
+}
+else{
+    document.write("you are ugly");
+}
